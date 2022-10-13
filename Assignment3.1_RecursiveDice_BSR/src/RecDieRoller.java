14fbc0edc541aa761bd9296854fa1e0d21c17a64
brentreynolds <bsreynol@iu.edu>
2022-10-12 21:26:48 -0400

    Finished the RecDieRoller class and the RecDieRollerDriver class. Also added javadocs.

---
+++ Assignment3.1_RecursiveDice_BSR/src/RecDieRoller.java
@@ -0,0 +1,34 @@
+
+/**
+ * This class contains the method that rolls the dice until it rolls the number 2
+ * It prints out each of the dice rolls including the number 2
+ * @author brentreynolds
+ * @version 10/11/22 Fall 2022
+ */
+public class RecDieRoller {
+	
+	private int numberOfRollsUntilTwo;
+
+	/**
+	 * This method rolls a dice until it rolls a 2. It prints out each of the roll attempts.
+	 * @return the number of 
+	 */
+	public int rollDie() {
+		numberOfRollsUntilTwo += 1;
+		
+		int number = (int) (Math.random() * 6) + 1;
+		
+		System.out.println(number);
+		
+		if(number != 2) {
+			return rollDie();
+		}
+		
+		else {
+			int numOfRolls = numberOfRollsUntilTwo;
+			numberOfRollsUntilTwo = 0;
+			return numOfRolls;
+		}
+		
+	}
+}
---
+++ Assignment3.1_RecursiveDice_BSR/src/RecDieRollerDriver.java
@@ -0,0 +1,30 @@
+
+/**
+ * This class calculates the average amount of rolls needed to roll the number 2 on a six sided dice
+ * @author brentreynolds
+ * @version 10/11/22 Fall 2022
+ */
+public class RecDieRollerDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int count = 1;
+		double sum = 0;
+		double average;
+		
+		RecDieRoller die = new RecDieRoller();
+		
+		while(count <= 1000) {
+			
+		sum += die.rollDie();
+		count++;
+		}
+		
+		average = sum / 1000;
+		
+		System.out.println("The average number of rolls it takes to roll a 2 is: " + average);
+
+	}
+
+}
